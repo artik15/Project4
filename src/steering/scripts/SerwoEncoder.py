c593ea33fab73bf9d1412c410cf538c4e680e70d
MariKat77 <mario.1999.mk@gmail.com>
2023-11-17 22:26:21 +0100

    Small update of SerwoEncoder file

--- src/steering/scripts/SerwoEncoder.py
+++ src/steering/scripts/SerwoEncoder.py
@@ -2,16 +2,21 @@ import smbus
 import RPi.GPIO as GPIO
 from Serwo_Parametrs import *
 from biny import *
+
 class SerwoEncoder:
+
     def __init__(self) -> None:
         self.first_angle = 0.0
+
     def readFirstAngle(self, bus):
-        self.first_angle = raw_angle(bus.read_i2c_block_data(device_address, re>
+        self.first_angle = raw_angle(bus.read_i2c_block_data(device_address, read_register_angle))
+
     def defineBus(file):
         bus = smbus.SMBus(file)
         return bus
+    
     def getAngle(self,bus):
-        angle = raw_angle(bus.read_i2c_block_data(device_address, read_register>
+        angle = raw_angle(bus.read_i2c_block_data(device_address, read_register_angle))
         calculated_angle = angle - self.first_angle
         return calculated_angle
 
